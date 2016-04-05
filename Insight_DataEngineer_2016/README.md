d6557bc62fc9b9f730db9d78183155d8e0d7d5d4
Sima Khashkhashi Moghaddam <khashkha@usc.edu>
2016-04-05 01:06:56 -0700

    DataEngineer2016

---
+++ Insight_DataEngineer_2016/README.md
@@ -0,0 +1,15 @@
+# Insight_DataScience-Project
+Clean Tweet text and calculate tweet degrees
+1- please put your input files at this path: ..\coding-challenge-master\tweet_input\tweet.txt
+2- you can find the output on this path:..\coding-challenge-master\tweet_output\ft1.txt and tf2.txt
+3- you can run the program directly from this path: ..\\coding-challenge-master\src\Run\InsightDataScience.exe
+4- you can open the project and build and if you want change the input and output path on TextExtraction.cs class
+
+
+Notification:
+1- I just tries to provide exactly the same tree for files and resources. 
+2- My program tries to pick up tweets.txt from tweet_input folder and will put the output.txt on tweet_output.
+3- sunning the run_tests causes some problem to me!
+4- easily just download my project and just run the exe file on run folder. Do not forgot to put your files on input path.
+5- if you want to see my project on Visual Studio and run it, you need NewtonSoft.Json which is a dll file and you can easily download it by NuGet.
+6- One Sample of the output already is located on tweet_output folder
---
+++ Insight_DataEngineer_2016/src/TextExtraction.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace Insight_DataEngineer_2016.src
+{
+    class TextExtraction
+    {
+        /// <summary>
+        /// \insight_testsuite\tests\test-2-tweets-all-distinct\tweet_input
+        /// I have considered three pathes for input and output files as constant
+        /// </summary>
+        private SlidingWindow _slidingWindows;
+        private const string _filePath = @"..\\insight_testsuite\tests\test-2-tweets-all-distinct\\tweet_input\tweets.txt";//@"C:\Users\sima\Documents\Visual Studio 2015\InsightDataScience\InsightDataScience\bin\Debug\tweets.txt";//@"..\\..\\coding-challenge-master\tweet_input\tweets.txt";
+        private const string _outPutTwoPath = @"..\\insight_testsuite\tests\test-2-tweets-all-distinct\\tweet_output\output.txt";// @"C:\Users\sima\Documents\Visual Studio 2015\InsightDataScience\InsightDataScience\bin\Debug\output.txt";// @"..\\..\\coding-challenge-master\tweet_output\output.txt";
+
+        /// <summary>
+        /// I am using the Newtonsoft.Json library to have easy access to json file attribures.
+        /// for having Newtonsoft libarary I have used manage NuGet option of Refrence menue.
+        /// </summary>
+        public TextExtraction()
+        {
+            _slidingWindows = new SlidingWindow();
+
+        }
+        /// <summary>
+        /// in this method I am trying to extarct two main attributes of each json row: text and create_at
+        /// then it calculates the degree by the help of SlidingWindow class.
+        /// 
+        /// the another important option about writing and reading from the files.
+        /// actually reagdring the clean and friendly coding rules it would be better to do these two tasks seperately
+        /// but I prefere to do both in one function becuase I do not like to use extra memory to save the results. 
+        /// on the other hands using events and delegates cuase many intra-communications between classes and functions which 
+        /// cause more time and rsources consuming. 
+        /// Finally:
+        /// I am extracting the text and date 
+        /// then I am writing them immidiately on output to have better memory management
+        /// </summary>
+        public void FindCleanTextAndDegree()
+        {
+            using (FileStream fs = File.Open(_filePath, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            using (StreamWriter sw2 = File.AppendText(_outPutTwoPath))
+            {
+                string s;
+                double degree = -1;
+
+
+                while ((s = sr.ReadLine()) != null)
+                {
+
+                    JObject json = JObject.Parse(s);
+                    string text = string.Empty;
+                    string date = string.Empty;
+                    foreach (KeyValuePair<String, JToken> app in json)
+                    {
+
+                        string appName = app.Key;
+                        if (appName == "created_at")
+                            date = (String)app.Value;
+
+
+
+
+                        else if (appName == "entities")
+                        {
+
+                            JObject json1 = (JObject)app.Value;
+                            List<string> tags = GetHashTaggedWords(json1);
+                            _slidingWindows.CheckSlidingWindowUpdate(tags, date);
+                            //double degree = _slidingWindows.GetDegree();
+                            //sw2.WriteLine(degree.ToString());
+
+
+
+
+                        }
+
+                    }
+                    degree = _slidingWindows.GetDegree();
+                    sw2.WriteLine(degree.ToString());
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Get Hash Tags words from text tags by the help of Regular Expression
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private List<string> GetHashTaggedWords(JObject json)
+        {
+            if (json == null)
+                return null;
+            List<string> tags = new List<string>();
+            JToken hashs = json.GetValue("hashtags");
+
+            foreach (JToken child in hashs.Children<JToken>())
+
+                foreach (JProperty nest in child.Children<JProperty>())
+                {
+                    if (nest.Name == "text")
+                        tags.Add(nest.Value.ToString());
+                }
+
+            return tags;
+
+        }
+
+
+
+    }
+}
